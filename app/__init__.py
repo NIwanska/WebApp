21064a41a5963d6a19a0797ffbe9c2aae6fa25c3
Julia Macuga <julia.macuga@gmail.com>
2024-04-28 12:19:01 +0200

    init migrations dir

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from .config import Config
 from .views import main, cart, products
 from .database import db
+from flask_migrate import Migrate
 
 
 def create_app():
     app = Flask(__name__)
+    migrate = Migrate(app, db)
     app.config.from_object(Config())
     db.init_app(app)
 
