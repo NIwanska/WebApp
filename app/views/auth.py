e946db550b6bd06f86e17d5d666aa77fcbaf1e8f
Julia Macuga <julia.macuga@gmail.com>
2024-04-28 14:03:59 +0200

    reformat auth.py

--- app/views/auth.py
+++ app/views/auth.py
@@ -27,31 +27,22 @@ def signup():
 
 @bp.route("/signup", methods=["POST"])
 def signup_post():
-    # code to validate and add user to database goes here
     email = request.form.get("email")
     username = request.form.get("username")
     password = request.form.get("password")
 
-    user = AuthUser.query.filter_by(
-        email=email
-    ).first()  # if this returns a user, then the email already exists in database
+    user = AuthUser.query.filter_by(email=email).first()
 
-    if (
-        user
-    ):  # if a user is found, we want to redirect back to signup page so user can try again
+    if user:
         return redirect(url_for("auth.signup"))
 
-    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
     new_user = AuthUser(
         email=email,
         username=username,
         password=generate_password_hash(password, method="pbkdf2:sha256"),
     )
-
-    # add the new user to the database
     db.session.add(new_user)
     db.session.commit()
-
     return redirect(url_for("auth.login"))
 
 
@@ -62,21 +53,15 @@ def login():
 
 @bp.route("/login", methods=["POST"])
 def login_post():
-    # login code goes here
     email = request.form.get("email")
     password = request.form.get("password")
     remember = True if request.form.get("remember") else False
 
     user = AuthUser.query.filter_by(email=email).first()
 
-    # check if the user actually exists
-    # take the user-supplied password, hash it, and compare it to the hashed password in the database
     if not user or not check_password_hash(user.password, password):
         flash("Please check your login details and try again.")
-        return redirect(
-            url_for("auth.login")
-        )  # if the user doesn't exist or password is wrong, reload the page
+        return redirect(url_for("auth.login"))
 
-    # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
     return redirect(url_for("main.home"))
