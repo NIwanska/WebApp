4156600844028ba55d5b9c08d2d8c86ba42db310
Julia Macuga <julia.macuga@gmail.com>
2024-05-07 23:38:56 +0200

    creaing order

--- app/views/order.py
+++ app/views/order.py
@@ -1,6 +1,7 @@
 from flask import render_template, Blueprint, request, flash, redirect
-from ..models import Address, db, ShoppingCart, Order
+from ..models import Address, db, ShoppingCart, Order, AuthUser
 from flask_login import login_required, current_user
+import datetime
 
 bp = Blueprint(
     "order",
@@ -14,61 +15,51 @@ bp = Blueprint(
 @bp.route("/")
 @login_required
 def index():
-    saved_address_list = Address.query.filter_by(user_id=current_user.id).all()
-
+    # saved_address_list = Address.query.filter_by(user_id=current_user.id).all()
+    saved_address_list = (
+        db.session.query(Address)
+        .join(Order, Address.id == Order.address_id)
+        .join(ShoppingCart, ShoppingCart.id == Order.cart_id)
+        .join(AuthUser, AuthUser.id == ShoppingCart.auth_user_id)
+        .filter((AuthUser.id == current_user.id))
+        .all()
+    )
     return render_template(
         "order/make_order.html", saved_address_list=saved_address_list
     )
 
 
-@bp.route("/add_address", methods=["GET"])
-@login_required
-def add_address_get():
-    return render_template("order/add_address.html")
-
-
-@bp.route("/add_address", methods=["POST"])
-@login_required
-def add_address():
-    street = request.form["street"]
-    city = request.form["city"]
-    country = request.form["country"]
-    zip_code = request.form["name"]
-
-    address = Address(
-        street=street,
-        city=city,
-        country=country,
-        zip_code=zip_code,
-        user_id=current_user.id,
-    )
-
-    db.session.add(address)
-    db.session.commit()
-    flash("Address added successfully!")
-    return redirect("/order")
-
-
 @bp.route("/submit_order", methods=["POST"])
 @login_required
 def submit_order():
-    address_id = request.form["address_id"]
-    cart = current_user.cart
+    if not request.form.get("saved_address_select", None):
+        street = request.form["street"]
+        city = request.form["city"]
+        country = request.form["country"]
+        zip_code = request.form["zip_code"]
+
+        address = Address(street=street, city=city, country=country, zip_code=zip_code)
+
+        db.session.add(address)
+        db.session.commit()
+
+    cart = ShoppingCart.query.filter_by(auth_user_id=current_user.id).first()
     delivery_method_id = request.form["delivery_method_id"]
     order_status_id = 1
-    datetime = datetime.datetime.utcnow()
-    total = ShoppingCart.total
+    datetime_val = datetime.datetime.utcnow()
+    total = cart.total
 
     order = Order(
-        address_id=address_id,
+        address_id=address.id,
         cart_id=cart.id,
         total=total,
-        datetime=datetime,
+        datetime=datetime_val,
         order_status_id=order_status_id,
         delivery_id=delivery_method_id,
     )
-    db.sesion.add(order)
+    db.session.add(order)
     db.session.commit()
+    flash("Order successfull!")
 
     return redirect("/order/success")
 
