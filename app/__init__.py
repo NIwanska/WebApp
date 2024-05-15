d86fc76745d1bf2fabeec4ed4d7f98abe41b1fcf
Natalia <natalia@pop-os.localdomain>
2024-05-15 19:31:27 +0200

    change data models

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ from .views import main, cart, products, order, auth
 from .database import db
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from .models import SizeType, Size, ProductType, ProductCategory, DeliveryMethod, OrderStatus
+import csv
+
+
 
 login_manager = LoginManager()
 
@@ -17,6 +21,7 @@ def create_app():
     with app.app_context():
         db.create_all()
         print("Db created")
+        add_data_to_sqlalchemy()
 
     login_manager.init_app(app)
     from .models import AuthUser
@@ -33,3 +38,79 @@ def create_app():
     app.register_blueprint(order.bp)
 
     return app
+
+
+    
+def add_data_to_sqlalchemy():
+
+    if not SizeType.query.first():
+        
+
+        with open('./csv_db/size_type.csv') as csv_file:
+            data = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in data:
+                if line_count == 0:
+                    line_count += 1
+                else:
+                    line_count += 1
+                    SizeType(id=int(row[0]), name=row[1]) 
+                    print(SizeType.query.first())
+        # db.session.commit()
+
+    if not Size.query.first():
+        with open('./csv_db/size.csv') as csv_file:
+            data = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in data:
+                if line_count == 0:
+                    line_count += 1
+                else:
+                    line_count += 1
+                    Size(id=int(row[0]), name=row[1], size_type_id=int(row[2])) 
+
+    if not ProductType.query.first():
+        with open('./csv_db/product_type.csv') as csv_file:
+            data = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in data:
+                if line_count == 0:
+                    line_count += 1
+                else:
+                    line_count += 1
+                    ProductType(id=int(row[0]), name=row[1], color=row[5], price=float(row[2]), img_url=row[3], product_category_id=int(row[4]))
+    
+    if not ProductCategory.query.first():
+        with open('./csv_db/product_category.csv') as csv_file:
+            data = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in data:
+                if line_count == 0:
+                    line_count += 1
+                else:
+                    line_count += 1
+                    ProductCategory(id=int(row[0]), subcategory_name=row[1], category_name=row[2], size_type_id=int(row[3]))
+    
+    if not DeliveryMethod.query.first():
+        with open('./csv_db/delivery_method.csv') as csv_file:
+            data = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in data:
+                if line_count == 0:
+                    line_count += 1
+                else:
+                    line_count += 1
+                    DeliveryMethod(id=int(row[0]), name=row[1], price=float(row[2]))
+    
+    if not OrderStatus.query.first():
+        with open('./csv_db/order_status.csv') as csv_file:
+            data = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in data:
+                if line_count == 0:
+                    line_count += 1
+                else:
+                    line_count += 1
+                    OrderStatus(id=int(row[0]), name=row[1])
+    db.session.commit()
+
