828240e75262bffb96dee5a7be8abb07eb0a90f7
Natalia <natalia@pop-os.localdomain>
2024-05-23 21:28:00 +0200

    add sidebar with category navigation

--- app/views/main.py
+++ app/views/main.py
@@ -1,5 +1,5 @@
 from flask import render_template, Blueprint
-from ..models import ProductItem
+from ..models import ProductType, ProductCategory
 from flask_login import current_user
 
 bp = Blueprint(
@@ -10,8 +10,12 @@ bp = Blueprint(
     url_prefix="/",
 )
 
-
 @bp.route("/")
-def home():
-    products = ProductItem.query.all()
-    return render_template("main/main.html", current_user=current_user)
+@bp.route("/category/<int:category_id>")
+def home(category_id=None):
+    categories = ProductCategory.query.order_by(ProductCategory.category_name, ProductCategory.subcategory_name).all()
+    if category_id:
+        products = ProductType.query.filter_by(product_category_id=category_id).all()
+    else:
+        products = ProductType.query.all()
+    return render_template("main/main.html", products=products, categories=categories, current_user=current_user)
