fe0f55614423aa86e5a733415028125d7f31c6b4
Julia Macuga <julia.macuga@gmail.com>
2024-05-29 16:57:59 +0200

    connecion context

--- insert_data.py
+++ insert_data.py
@@ -9,11 +9,9 @@ if __name__ == "__main__":
         f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@localhost:{os.getenv('DB_PORT')}/app_db"
     )
     engine = create_engine(SQLALCHEMY_DATABASE_URI)
-    connection = engine.connect()
-
     product_monthly_reports = pd.read_csv("./csv_db/products_monthly_reports.csv")
-    print(f"product_monthly_reports: {product_monthly_reports.head()}")
 
-    res = product_monthly_reports.to_sql("product_monthly_reports", connection, if_exists="append", index=False)
-    print(f"Affected {res} rows")
-    connection.close()
+    with engine.connect() as connection:
+        print(f"product_monthly_reports: {product_monthly_reports.head()}")
+        product_monthly_reports.to_sql("product_monthly_reports", connection, if_exists="append", index=False)
+        print("Data inserted successfully")
