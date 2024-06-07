25a9dd6e2d2c057fe6cc80dbf5296a775cb0c50e
Natalia <natalia@pop-os.localdomain>
2024-06-07 14:13:08 +0200

    add color filtering

--- app/views/main.py
+++ app/views/main.py
@@ -1,6 +1,7 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request, url_for, redirect
 from ..models import ProductType, ProductCategory
 from flask_login import current_user
+from sqlalchemy import distinct
 
 bp = Blueprint(
     "main",
@@ -11,11 +12,43 @@ bp = Blueprint(
 )
 
 @bp.route("/")
-@bp.route("/category/<int:category_id>")
-def home(category_id=None):
+@bp.route("/category/<string:category_name>")
+@bp.route("/subcategory/<int:subcategory_id>")
+def home(category_name=None, subcategory_id=None):
+    page = request.args.get('page', 1, type=int)
+    color_filter = request.args.get('color', None)
+
     categories = ProductCategory.query.order_by(ProductCategory.category_name, ProductCategory.subcategory_name).all()
-    if category_id:
-        products = ProductType.query.filter_by(product_category_id=category_id).all()
-    else:
-        products = ProductType.query.all()
-    return render_template("main/main.html", products=products, categories=categories, current_user=current_user)
+
+    query = ProductType.query
+    if category_name:
+        # Pobierz wszystkie podkategorie w ramach tej głównej kategorii
+        subcategories = ProductCategory.query.filter_by(category_name=category_name).all()
+        subcategory_ids = [subcategory.id for subcategory in subcategories]
+        query = query.filter(ProductType.product_category_id.in_(subcategory_ids))
+    elif subcategory_id:
+        query = query.filter_by(product_category_id=subcategory_id)
+    if color_filter:
+        query = query.filter_by(color=color_filter)
+
+    total_products = query.count()
+    products = query.offset((page - 1) * 9).limit(9).all()
+
+    colors = ProductType.query.with_entities(distinct(ProductType.color)).order_by(ProductType.color).all()
+
+    pagination = {
+        'page': page,
+        'per_page': 9,
+        'total': total_products,
+        'pages': (total_products + 9 - 1) // 9,  # number of pages
+    }
+
+    return render_template("main/main.html", 
+                           products=products, 
+                           categories=categories, 
+                           current_user=current_user,
+                           colors=[color[0] for color in colors],
+                           pagination=pagination,
+                           color_filter=color_filter,
+                           category_name=category_name,
+                           subcategory_id=subcategory_id)
