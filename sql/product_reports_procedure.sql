d25b8681808c00c7bc4e4023d0384ba6873bd5f1
mkrajew4 <01171632@pw.edu.pl>
2024-06-21 12:07:15 +0200

    corrected product monthly reports

--- sql/product_reports_procedure.sql
+++ sql/product_reports_procedure.sql
@@ -6,8 +6,7 @@ CREATE OR REPLACE PROCEDURE product_reports (
     p_product_id INTEGER,
     p_month VARCHAR,
     p_year INTEGER,
-    p_count INTEGER,
-    p_total DOUBLE PRECISION
+    p_count INTEGER
 )
 LANGUAGE plpgsql
 AS $$
@@ -15,6 +14,8 @@ DECLARE
     v_id INTEGER;
     v_count INTEGER;
     v_total INTEGER;
+    v_product_type_id INTEGER;
+    v_price DOUBLE PRECISION;
 BEGIN
     SELECT id, count, total
     INTO v_id, v_count, v_total
@@ -23,12 +24,23 @@ BEGIN
     ORDER BY year, month DESC
     LIMIT 1;
 
+    SELECT product_type_id
+    INTO v_product_type_id
+    FROM product_item
+    WHERE id = p_product_id
+    LIMIT 1;
+
+    SELECT price
+    INTO v_price
+    FROM product_type
+    WHERE id = v_product_type_id;
+
     IF v_id IS NULL THEN
         INSERT INTO product_monthly_reports (id, product_item_id, month, year, count, total)
-        VALUES (DEFAULT, p_product_id, p_month, p_year, p_count, p_total);
+        VALUES (DEFAULT, p_product_id, p_month, p_year, p_count, ROUND((p_count*v_price)::NUMERIC,2));
     ELSE
         UPDATE product_monthly_reports
-        SET count = v_count+p_count, total=v_total+p_total
+        SET count = v_count+p_count, total=ROUND((v_total+p_count*v_price)::NUMERIC,2)
         WHERE id = v_id;
     END IF;
 END;
