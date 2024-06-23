a05b93258e0f07679932c7df02b1a1a2a17b5d9c
Paulina <paudabr@gmail.com>
2024-06-23 23:54:50 +0200

    add "no information"

--- app/views/admin.py
+++ app/views/admin.py
@@ -22,9 +22,10 @@ def admin_view_products(year=2024):
     years = [i for i in range(lowest_year, actual_year+1)]
     months = [str(i) for i in range(1, 13)]
     months_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    products_monthly_reports_hist(year, months, months_names)
-    products_monthly_reports_pie(year, months, months_names)
-    return render_template("admin/admin_products.html", years=years, months=months_names, year=year)
+    is_info_products = products_monthly_reports_hist(year, months, months_names)
+    is_info_categories = products_monthly_reports_pie(year, months, months_names)
+    return render_template("admin/admin_products.html", years=years, months=months_names, year=year, 
+                           is_info_products=is_info_products, is_info_categories=is_info_categories)
 
 @bp.route("/cities/<int:year>/")
 def admin_view_cities(year=2024):
@@ -33,8 +34,8 @@ def admin_view_cities(year=2024):
     years = [i for i in range(lowest_year, actual_year+1)]
     months = [str(i) for i in range(1, 13)]
     months_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    cities_monthly_reports_hist(year, months, months_names)
-    return render_template("admin/admin_cities.html", years=years, months=months_names, year=year)
+    is_info = cities_monthly_reports_hist(year, months, months_names)
+    return render_template("admin/admin_cities.html", years=years, months=months_names, year=year, is_info=is_info)
 
 @bp.route("/invoices/<int:year>/")
 def admin_view_invoices(year=2024):
@@ -43,5 +44,5 @@ def admin_view_invoices(year=2024):
     years = [i for i in range(lowest_year, actual_year+1)]
     months = [str(i) for i in range(1, 13)]
     months_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    invoices_monthly_reports_hist(year, months, months_names)
-    return render_template("admin/admin_invoices.html", years=years, months=months_names, year=year)
+    is_info = invoices_monthly_reports_hist(year, months, months_names)
+    return render_template("admin/admin_invoices.html", years=years, months=months_names, year=year, is_info=is_info)
